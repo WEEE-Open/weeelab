c34f563be1d3d98ecdcef1d0a0710cbb75222e63
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2017-11-15 23:01:49 +0100

    Login and logout scripts
    For real this time

---
+++ login.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+RETURNED=1337
+while [[ $RETURNED -ne 0 ]]; do
+	echo "Type your name.surname OR id (matricola) without initial 's' OR nickname:"
+	read name
+	weeelab login $name
+	RETURNED=$?
+done
+
