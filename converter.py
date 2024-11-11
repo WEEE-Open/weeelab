c8df9a9851514861b7c87985427c2fa548018250
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-11-11 17:59:47 +0100

    better handling of  broken logs

--- converter.py
+++ converter.py
@@ -4,6 +4,7 @@ Function to convert the old log txt files to sql files
 
 from datetime import datetime
 from pathlib import Path
+import traceback
 
 
 def convert_to_sql(file):
@@ -12,10 +13,10 @@ def convert_to_sql(file):
 
     with open(file, "r") as f:
         lines = f.readlines()
-    sql_file = file.with_suffix(".sql")
 
-    with open(sql_file, "w") as f:
-        for line in lines:
+    sql_file = file.with_suffix(".sql")
+    for i, line in enumerate(lines):
+        try:
             indatetime, exdatetime = line[1:17], line[20:36]
             in_timestamp = int(
                 datetime.strptime(indatetime, "%d/%m/%Y %H:%M").timestamp()
@@ -24,12 +25,18 @@ def convert_to_sql(file):
                 datetime.strptime(exdatetime, "%d/%m/%Y %H:%M").timestamp()
             )
             user, task = line[47:-1].split("> :: ", 1)
-            f.write(
-                f"INSERT INTO audit (userId, startTime, endTime, motivation, approved, location) VALUES ('{user}', {in_timestamp}, {ex_timestamp}, '{task}', true, 'lab');\n"
+            line = (
+                f"INSERT INTO audit (userId, startTime, endTime, motivation, approved, location) VALUES ('{user}', {in_timestamp}, {ex_timestamp}, '{task}', true, 'lab');"
             )
-    return sql_file
+        except ValueError:
+            print(f"Error in {file} at line {i}: {line}")
+            print(traceback.format_exc(0))
+            break
+    else:
+        with open(sql_file, "w") as f:
+            f.writelines(lines)
 
 
 if __name__ == "__main__":
-    for file in Path(".").glob("*.txt"):
+    for file in Path(".").glob("log*.txt"):
         convert_to_sql(file)
