5b9a8417950073039b41a6922784643cf7e6a506
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-11-14 16:21:17 +0100

    take ldap uuids for conversion to grillo's db

--- converter.py
+++ converter.py
@@ -5,7 +5,17 @@ Function to convert the old log txt files to sql files
 from datetime import datetime
 from pathlib import Path
 import traceback
+import ldap
+from ldap.filter import escape_filter_chars
+from weeelab import LDAP_BIND_DN, LDAP_PASSWORD
 
+DRY_RUN = True
+LDAP_HOST = "ldap://ldap.example.com"
+LDAP_BIND_DN = "cn=something,dc=example,dc=com"
+LDAP_PASSWORD = "foo"
+LDAP_TREE = "ou=People,dc=example,dc=com"
+
+users = set()
 
 def convert_to_sql(file, dry_run=False):
     if isinstance(file, str):
@@ -32,6 +42,7 @@ def convert_to_sql(file, dry_run=False):
             new_lines.append(
                 f"INSERT INTO audit (userId, startTime, endTime, motivation, approved, location) VALUES ('{user}', {in_timestamp}, {ex_timestamp}, '{task}', true, 'lab');\n"
             )
+            users.add(user)
         except ValueError:
             print(f"Error in {file} at line {i}: {line}")
             print(traceback.format_exc(0))
@@ -44,4 +55,44 @@ def convert_to_sql(file, dry_run=False):
 
 if __name__ == "__main__":
     for file in Path(".").glob("log*.txt"):
-        convert_to_sql(file)
+        convert_to_sql(file, DRY_RUN)
+
+    new_lines = []
+    for user in users:
+        # Get ldap uuid
+        filters = (
+            f"(&(objectClass=weeeOpenPerson)(schacpersonaluniquecode={escape_filter_chars(user)})(!(nsaccountlock=true)))",
+        )
+
+        conn = ldap.initialize(LDAP_HOST)
+        conn.protocol_version = ldap.VERSION3
+        if LDAP_HOST.startswith("ldap://"):
+            conn.start_tls_s()
+        conn.simple_bind_s(LDAP_BIND_DN, LDAP_PASSWORD)
+
+
+        for ldap_filter in filters:
+            result = conn.search_s(
+                LDAP_TREE,
+                ldap.SCOPE_SUBTREE,
+                ldap_filter,
+                ("weeeopenuniqueid", "uid", "cn", "givenname", "signedsir"),
+            )
+            if len(result) > 1:
+                ambiguous = True
+            if len(result) == 1:
+                attr = result[0][1]
+                if "signedsir" in attr:
+                    signed_sir = attr["signedsir"][0].decode().lower() == "true"
+                else:
+                    signed_sir = False
+                uuid = attr["weeeopenuniqueid"][0].decode()
+        conn.unbind_s()
+
+
+        new_lines.append(f"INSERT INTO users (id, seconds, inlab) VALUES ('{uuid}', 0, false);\n")
+
+    if not DRY_RUN:
+        with open('init_db.sql', "a") as f:
+            f.writelines(new_lines)
+            f.write("INSERT INTO locations (name) VALUES ('lab');\n")
