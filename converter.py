d40774f097f99ea3e16c4b86689d4dddc27a7701
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2025-05-17 12:22:06 +0200

    fix converter

--- converter.py
+++ converter.py
@@ -3,29 +3,40 @@ Function to convert the old log txt files to sql files
 """
 
 from datetime import datetime
+import os
 from pathlib import Path
 import traceback
 import ldap
 from ldap.filter import escape_filter_chars
-from weeelab import LDAP_BIND_DN, LDAP_PASSWORD
+from dotenv import load_dotenv
+import logging
 
-DRY_RUN = True
-LDAP_HOST = "ldap://ldap.example.com"
-LDAP_BIND_DN = "cn=something,dc=example,dc=com"
-LDAP_PASSWORD = "foo"
-LDAP_TREE = "ou=People,dc=example,dc=com"
+logger = logging.getLogger(__name__)
 
-users = set()
+load_dotenv(os.path.join(os.path.dirname(os.path.realpath(__file__)), '.env'))
 
-def convert_to_sql(file, dry_run=False):
+LDAP_HOST = os.getenv("LDAP_HOST")
+LDAP_BIND_DN = os.getenv("LDAP_BIND_DN")
+LDAP_PASSWORD = os.getenv("LDAP_PASSWORD")
+LDAP_TREE = "ou=People,dc=weeeopen,dc=it"
+
+users = {}
+uuids = {}
+new_lines = []
+
+conn = ldap.initialize(LDAP_HOST)
+conn.protocol_version = ldap.VERSION3
+if LDAP_HOST.startswith("ldap://"):
+    conn.start_tls_s()
+conn.simple_bind_s(LDAP_BIND_DN, LDAP_PASSWORD)
+
+def convert_to_sql(file):
     if isinstance(file, str):
         file = Path(file)
 
     with open(file, "r") as f:
         lines = f.readlines()
 
-    sql_file = file.with_suffix(".sql")
-    new_lines = []
     for i, line in enumerate(lines):
         try:
             indatetime, exdatetime, duration = line[1:17], line[20:36], line[39:44]
@@ -39,61 +50,53 @@ def convert_to_sql(file, dry_run=False):
             if d_time*60 != ex_timestamp - in_timestamp:
                 raise ValueError(f"Duration does not match. Explicit is {duration}, but time difference is {(ex_timestamp - in_timestamp)//3600}:{(ex_timestamp - in_timestamp)//60 % 60}")
             user, task = line[47:-1].split("> :: ", 1)
-            new_lines.append(
-                f"INSERT INTO audit (userId, startTime, endTime, motivation, approved, location) VALUES ('{user}', {in_timestamp}, {ex_timestamp}, '{task}', true, 'lab');\n"
-            )
-            users.add(user)
+            if user in uuids:
+                uuid = uuids[user]
+            else:
+                uuid = get_uuid_from_user(user)
+                uuids[user] = uuid
+
+            new_lines.append((uuid, in_timestamp, ex_timestamp, task))
+            if uuid not in users:
+                users[uuid] = 0
+
+            users[uuid] += ex_timestamp - in_timestamp
+
         except ValueError:
-            print(f"Error in {file} at line {i}: {line}")
-            print(traceback.format_exc(0))
-            break
-    else:
-        if not dry_run:
-            with open(sql_file, "w") as f:
-                f.writelines(new_lines)
+            print(f"Error in {file} at line {i+1}: {line}", end="")
+            logger.exception(traceback.format_exc(0))
+
+def get_uuid_from_user(user):
+    # Get ldap uuid
+    ldap_filter = f"(&(objectClass=weeeOpenPerson)(uid={escape_filter_chars(user)}))"
+
+
+    result = conn.search_s(
+        LDAP_TREE,
+        ldap.SCOPE_ONELEVEL,
+        ldap_filter,
+        ("schacPersonalUniqueCode",),
+    )
+    if len(result) > 1:
+        ambiguous = True
+        logger.exception(f"Ambiguous search ({ldap_filter}): {result}")
+    elif len(result) == 1:
+        attr = result[0][1]
+        uuid = attr["schacPersonalUniqueCode"][0].decode()
+    return uuid
 
 
 if __name__ == "__main__":
+    print("INSERT INTO locations (name) VALUES ('lab');")
+
     for file in Path(".").glob("log*.txt"):
-        convert_to_sql(file, DRY_RUN)
+        convert_to_sql(file)
+    conn.unbind_s()
 
-    new_lines = []
-    for user in users:
-        # Get ldap uuid
-        filters = (
-			f"(&(objectClass=weeeOpenPerson)(uid={escape_filter_chars(user)})(!(nsaccountlock=true)))",
-			f"(&(objectClass=weeeOpenPerson)(weeelabnickname={escape_filter_chars(user)})(!(nsaccountlock=true)))"
-		)
+    for user, seconds in users.items():
+        print(f"INSERT INTO user (id, seconds, inlab) VALUES ('{user}', {seconds}, false);")
 
-        conn = ldap.initialize(LDAP_HOST)
-        conn.protocol_version = ldap.VERSION3
-        if LDAP_HOST.startswith("ldap://"):
-            conn.start_tls_s()
-        conn.simple_bind_s(LDAP_BIND_DN, LDAP_PASSWORD)
+    for line in new_lines:
+        print(f"INSERT INTO audit (userId, startTime, endTime, motivation, approved, location) VALUES ('{line[0]}', {line[1]}, {line[2]}, '{line[3]}', true, 'lab');")
 
 
-        for ldap_filter in filters:
-            result = conn.search_s(
-                LDAP_TREE,
-                ldap.SCOPE_SUBTREE,
-                ldap_filter,
-                ("weeeopenuniqueid", "uid", "cn", "givenname", "signedsir"),
-            )
-            if len(result) > 1:
-                ambiguous = True
-            if len(result) == 1:
-                attr = result[0][1]
-                if "signedsir" in attr:
-                    signed_sir = attr["signedsir"][0].decode().lower() == "true"
-                else:
-                    signed_sir = False
-                uuid = attr["weeeopenuniqueid"][0].decode()
-        conn.unbind_s()
-
-
-        new_lines.append(f"INSERT INTO users (id, seconds, inlab) VALUES ('{uuid}', 0, false);\n")
-
-    if not DRY_RUN:
-        with open('init_db.sql', "a") as f:
-            f.writelines(new_lines)
-            f.write("INSERT INTO locations (name) VALUES ('lab');\n")
